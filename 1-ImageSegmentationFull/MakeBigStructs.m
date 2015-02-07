4ace6520fcd78d0ec8924fb39099a8d12cc6e15e
Roswitha Gostner <r.gostner@gmail.com>
2015-02-07 15:59:44 +0000

    make structure data: now you only have to select the folder, and no longer move around any program. it works from the project folder.

--- 1-ImageSegmentationFull/MakeBigStructs.m
+++ 1-ImageSegmentationFull/MakeBigStructs.m
@@ -1,20 +1,30 @@
 %Put me in your contour output folder from CSG
 
-N=1; %Number of stacks
+folder = uigetdir(matlabroot,'Select Experiment Folder');
+dirData=dir( fullfile(folder,'CellFrameData*.mat') );
+dirIndex = [dirData.isdir];  %# Find the index for directories
+fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
+if ~isempty(fileList)
+    fileList = cellfun(@(x) fullfile(folder,x),...  %# Prepend path to files
+    fileList,'UniformOutput',false);
+end
+N = length(fileList);
+if N<1, error('No Files found in Experiment folder, stopped without processing');
+    exit(0); end
 
-load('CellFrameData001.mat')
-BigCellDataStruct=CellFrameData;
-for i=2:N
-    load(sprintf('CellFrameData%03d',i))
+BigCellDataStruct=[];
+for i=1:N
+    load(char(fileList(i)));
     if ~isempty(fieldnames(CellFrameData))
         BigCellDataStruct=[BigCellDataStruct, CellFrameData];
-    else
-    i
     end
 clear('CellFrameData')
 end
 
-save -v7.3 BigCellDataStruct.mat BigCellDataStruct
+sFile = fullfile(folder, 'BigCellDataStruct.mat');
+save( sFile, 'BigCellDataStruct', '-v7.3');
+
+
 
 L=length(BigCellDataStruct);
 BigCellArray=BigCellDataStruct(1).Contours';
@@ -24,4 +34,5 @@ for i=2:L
     cell_indices=[cell_indices; i*ones(length(BigCellDataStruct(i).Contours),1)];
 end
 
-save('Bigcellarrayandindex.mat', 'BigCellArray', 'cell_indices', '-v7.3')
+sFile = fullfile(folder, 'Bigcellarrayandindex.mat');
+save(sFile, 'BigCellArray', 'cell_indices', '-v7.3');
