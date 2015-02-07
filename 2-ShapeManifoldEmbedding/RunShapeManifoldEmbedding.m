0c031d9ac57f96e8f54b7ab6ad2a6197878320dc
Roswitha Gostner <r.gostner@gmail.com>
2015-02-07 16:26:10 +0000

    added message if file does not exists in the folder.

--- 2-ShapeManifoldEmbedding/RunShapeManifoldEmbedding.m
+++ 2-ShapeManifoldEmbedding/RunShapeManifoldEmbedding.m
@@ -1,8 +1,14 @@
 function  RunShapeManifoldEmbedding()
 folder = uigetdir(matlabroot,'Select Experiment Folder');
-dataFile = fullfile(folder, 'Bigcellarrayandindex');
-data = load(dataFile);
-ShapeManifoldEmbedding_finalSJ(data.BigCellArray, folder);
+dataFile = fullfile(folder, 'Bigcellarrayandindex.mat');
+if exist(dataFile, 'file')
+    data = load(dataFile);
+    ShapeManifoldEmbedding_finalSJ(data.BigCellArray, folder);
+else 
+    display('-------')
+    display('The file "Bigcellarrayandindex.mat" does not exist in your Experiment folder.');
+    display('Please check whether previous steps have been succesfully completed.');
+    display('-------')
 
 end
 
