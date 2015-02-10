63f7e6b1a0be1dddc99ea2d269fa077d825828aa
Roswitha Gostner <r.gostner@gmail.com>
2015-02-10 19:33:55 +0000

    Make big structure without manual adaptions.

---
+++ 1-ImageSegmentationFull/MakeBigStructsWithoutManual.m
@@ -0,0 +1,37 @@
+%Put me in your imagestacks folder from CSG
+
+folder = uigetdir(matlabroot,'Select Experiment Folder');
+dirData=dir( fullfile(folder,'CellFrameData*.mat') );
+dirIndex = [dirData.isdir];  %# Find the index for directories
+fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
+if ~isempty(fileList)
+    fileList = cellfun(@(x) fullfile(folder,x),...  %# Prepend path to files
+    fileList,'UniformOutput',false);
+end
+N = length(fileList);
+if N<1, error('No Files found in Experiment folder, stopped without processing');
+    exit(0); end
+
+BigCellDataStruct=[];
+for i=1:N
+    load(sprintf('ImageStack%03dCurveData.mat',i))
+    num_cells=max(cell2mat(Cell_numbers));
+    num_frames=length(Frame_curves);
+    for j=1:num_cells
+        for k=1:num_frames
+            CellArray={};
+            if ismember(j,Cell_numbers{k})
+                BigCellArray=[BigCellArray Frames_curves{k}(Cell_numbers{k}==j)];
+                CellArray=[CellArray Frames_curves{k}(Cell_numbers{k}==j)];
+                cell_indices=[cell_indices j+running_cell_indices];
+            end
+            CellDataStruct=struct('Stack_number',i,'Cell_numbers',k,'Contours',CellArray);
+        end
+        BigCellDataStruct=[BigCellDataStruct, CellFrameData];
+    end
+    
+end
+
+save -v7.3 BigCellDataStruct.mat BigCellDataStruct
+
+save('Bigcellarrayandindex.mat', 'BigCellArray', 'cell_indices', '-v7.3')
