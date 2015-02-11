022d029e3f668d214044b0c79d69f3f6187e29e8
Roswitha Gostner <r.gostner@gmail.com>
2015-02-11 14:00:04 +0000

    combined space slicer figure is drawing

--- 4-Shape_Averaging/SpaceSlicer.m
+++ 4-Shape_Averaging/SpaceSlicer.m
@@ -2,7 +2,7 @@ function [ idx, bounds ] = SpaceSlicer(CellShapeData)
 %SPACESLICER Summary of this function goes here
 %   Detailed explanation goes here
 %
-
+close all
 N=length(CellShapeData.point);
 if isfield(CellShapeData.set,'SCORE')
     SCORE=CellShapeData.set.SCORE;
@@ -12,32 +12,68 @@ else
     end
 end
 
-x_slices=4;
-y_slices=6;
+x_slices=6;
+y_slices=5;
+p=x_slices+1:(x_slices+1)*(y_slices+1);
+idx=find(mod(p,x_slices+1)==0);
+p(idx)=[];
+
 
 figure % x figure
-b1=slicey_magoo( CellShapeData,SCORE, [1 0], x_slices, true);
+
+[b1, xShapes]=slicey_magoo( CellShapeData,SCORE, [1 0], x_slices, true);
 
 figure % y figure
-b2=slicey_magoo( CellShapeData,SCORE, [0 1], y_slices, true);
+[b2, yShapes]=slicey_magoo( CellShapeData,SCORE, [0 1], y_slices, true);
 
 figure
+set(gcf,'color','w');
+for i=1:x_slices
+    subplot(y_slices+1, x_slices+1,i)
+    s=xShapes{i};
+    if ~isempty(s)
+        plot(s);
+    end
+    set(gca,'xcolor','w','ycolor','w','xtick',[],'ytick',[])
+end
+
+
+for i=1:y_slices
+    subplot(y_slices+1, x_slices+1,(i+1)*(x_slices+1));
+    idx = y_slices-i+1;
+    s=yShapes{idx};
+    if ~isempty(s)
+        plot(s);
+    end
+    set(gca,'xcolor','w','ycolor','w','xtick',[],'ytick',[])
+end
+
+
+
+subplot(y_slices+1, x_slices+1,p);
 plot(SCORE(:,1),SCORE(:,2),'.')
 hold on
+xlim([b1(1) b1(end)]);
+ylim([b2(1) b2(end)]);
 xm =xlim;
 ym =ylim;
+
 for i=2:x_slices
    plot([b1(i) b1(i)],ym,'color',[1 0.5 0]);
 end
+
+
 for i=2:y_slices
-   plot(xm,[b2(i) b2(i)],'color',[1 0 1]); 
+   plot(xm,[b2(i) b2(i)],'color',[1 0 1]);
 end
+
 plot(SCORE(:,1),SCORE(:,2),'.')
 %axis equal; axis tight
 
 end
 
-function [bounds]=slicey_magoo( CSD,SCORE, vect, num, plotshapes )
+function [bounds, avshapes]=slicey_magoo( CSD,SCORE, vect, num, plotshapes )
+avshapes={};
 N=length(CSD.point);
 d=length(vect);
 Score=SCORE(:,1:d);
@@ -65,7 +101,9 @@ end
 if plotshapes
     for i=1:num
         cellset_idx=find(idx==i);
-        if isempty(cellset_idx), continue; end
+        if isempty(cellset_idx), 
+            avshapes{i}=[];
+            continue; end
         [~,mid]=min(abs(normproj_d(cellset_idx)));
         avshape=shapemean(CSD,cellset_idx,cellset_idx(mid),0)';
         c=princomp([real(avshape) imag(avshape)]);
@@ -74,6 +112,7 @@ if plotshapes
         avshape=-avshape*sign(max(real(avshape))+min(real(avshape)));
         avshape=avshape*exp(1i*(pi/4));
         avshape=0.5*step*avshape/(1.1*max(abs(avshape)));
+        avshapes{i}=avshape;
         plot(avshape+centres(i))
         hold on
     end
