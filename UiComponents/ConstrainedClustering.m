e08d556f88a96f513f183a0fc4557462ff0f6030
Roswitha Gostner <r.gostner@gmail.com>
2015-02-16 15:11:13 +0000

    Extended Affinity propagation got a whole new graphical interface.

---
+++ UiComponents/ConstrainedClustering.m
@@ -0,0 +1,171 @@
+function varargout = ConstrainedClustering(varargin)
+% CONSTRAINEDCLUSTERING MATLAB code for ConstrainedClustering.fig
+%      CONSTRAINEDCLUSTERING, by itself, creates a new CONSTRAINEDCLUSTERING or raises the existing
+%      singleton*.
+%
+%      H = CONSTRAINEDCLUSTERING returns the handle to a new CONSTRAINEDCLUSTERING or the handle to
+%      the existing singleton*.
+%
+%      CONSTRAINEDCLUSTERING('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in CONSTRAINEDCLUSTERING.M with the given input arguments.
+%
+%      CONSTRAINEDCLUSTERING('Property','Value',...) creates a new CONSTRAINEDCLUSTERING or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before ConstrainedClustering_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to ConstrainedClustering_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help ConstrainedClustering
+
+% Last Modified by GUIDE v2.5 16-Feb-2015 13:40:36
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ConstrainedClustering_OpeningFcn, ...
+                   'gui_OutputFcn',  @ConstrainedClustering_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before ConstrainedClustering is made visible.
+function ConstrainedClustering_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ConstrainedClustering (see VARARGIN)
+
+% Choose default command line output for ConstrainedClustering
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+cPath=mfilename('fullpath');
+[folder,~,~] = fileparts(cPath);
+imFile = fullfile(folder, 'img/', 'header.png');
+img=imread(imFile);
+imshow(img,'Parent',handles.axes1)
+imFile = fullfile(folder, 'img/', 'cluster.png');
+img=imread(imFile);
+imshow(img,'Parent',handles.axes2)
+
+% UIWAIT makes ConstrainedClustering wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ConstrainedClustering_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[FileName,PathName] = uigetfile('*.mat','Select the MATLAB CellShapeData.mat file');
+fPath=fullfile(PathName, FileName);
+set(handles.edit1, 'String','...')
+if exist(fPath, 'file') % set file name, if it is correct file
+    if checkIfCorrectFilepath(fPath)==1
+      set(handles.edit1, 'String',fPath)  
+    end
+end
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fpath= get(handles.edit1, 'String');
+classes=get(handles.popupmenu1,'Value');
+[folder,~,~] = fileparts(fpath);
+if checkIfCorrectFilepath(fpath)==1
+   data = load(fpath);
+   ordered_list(classes,data.CellShapeData, folder);
+end
+
+
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function bool = checkIfCorrectFilepath(path)
+bool =0;
+try
+    data = load(path);
+    a=data.CellShapeData;
+    bool=1;
+catch 
+        mode = struct('WindowStyle','non-modal','Interpreter','tex');
+        msg = DialogMessages(1);
+        errordlg(msg, 'Error', mode);
+end
---
+++ UiComponents/DialogMessages.m
@@ -0,0 +1,24 @@
+% this is a method to keep all dialog messages in one place
+% it has the advantage of correcting messages in one place
+% and if error messages are re-used, they will all be corrected
+% in a single point... 
+% 1 - Error Msg for not choosing CellShapeData.mat
+
+function msg = DialogMessages( number )
+msgs=setUpMsgs();
+try
+msg = msgs{number};
+catch
+    msg ='Message Not found.';
+end
+end
+
+
+
+function msgs=setUpMsgs()
+msgs={};
+msgs{1} =['Selected File did not contain  the structure CellShapeData.' char(10) ...
+            'Please select the file "CellShapeData.mat" from previous step.'];
+msgs{2} =['Selected Number was greater than maximal clusters found in Dendogram.' char(10) ...
+            'Please select a more sensible number.'];
+end
