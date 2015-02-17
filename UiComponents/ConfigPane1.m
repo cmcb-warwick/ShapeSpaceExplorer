f5e4e41ed91ab700b6f424683a192097714c28e1
Roswitha Gostner <r.gostner@gmail.com>
2015-02-17 15:24:33 +0000

    added a new gui for the runMeFirst line.

---
+++ UiComponents/ConfigPane1.m
@@ -0,0 +1,155 @@
+function varargout = ConfigPane1(varargin)
+% CONFIGPANE1 MATLAB code for ConfigPane1.fig
+%      CONFIGPANE1, by itself, creates a new CONFIGPANE1 or raises the existing
+%      singleton*.
+%
+%      H = CONFIGPANE1 returns the handle to a new CONFIGPANE1 or the handle to
+%      the existing singleton*.
+%
+%      CONFIGPANE1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in CONFIGPANE1.M with the given input arguments.
+%
+%      CONFIGPANE1('Property','Value',...) creates a new CONFIGPANE1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before ConfigPane1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to ConfigPane1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help ConfigPane1
+
+% Last Modified by GUIDE v2.5 17-Feb-2015 14:38:36
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ConfigPane1_OpeningFcn, ...
+                   'gui_OutputFcn',  @ConfigPane1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before ConfigPane1 is made visible.
+function ConfigPane1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ConfigPane1 (see VARARGIN)
+
+% Choose default command line output for ConfigPane1
+handles.output = hObject;
+guidata(hObject, handles);
+cPath=mfilename('fullpath');
+[folder,~,~] = fileparts(cPath);
+imFile = fullfile(folder, 'img/', 'header.png');
+img=imread(imFile);
+imshow(img,'Parent',handles.axes1)
+set(handles.edit1, 'String','...') 
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes ConfigPane1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ConfigPane1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+folder =uigetdir(matlabroot,'Select Video Directory');
+set(handles.edit1, 'String',folder)  
+dir_struct = dir(folder);
+dir_struct =filterFiles(dir_struct, '.dv');
+[sorted_names,sorted_index] = sortrows({dir_struct.name}');
+handles.file_names = sorted_names;
+handles.is_dir = [dir_struct.isdir];
+handles.sorted_index = sorted_index;
+
+jList = java.util.ArrayList;% any java.util.List will be ok
+for i =1:length(sorted_index)
+    jList.add(sorted_index(i)-1, char(sorted_names(i)));
+end
+global jCBList;
+jCBList = com.mathworks.mwswing.checkboxlist.CheckBoxList(jList);
+jScrollPane = com.mathworks.mwswing.MJScrollPane(jCBList);
+javacomponent(jScrollPane,[10,10,300,200],gcf);
+set(jCBList, 'ValueChangedCallback', @myMatlabCallbackFcn);
+
+jCBModel= jCBList.getCheckModel;
+jCBModel.checkAll;
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% remove hiden files and folders
+function dir_struct =filterFiles(dir_struct, fileSuff)
+idx=[];
+suffLen=length(fileSuff);
+for i=1:length(dir_struct)
+    st = dir_struct(i);
+    nameLen = length(st.name);
+    if nameLen<=suffLen, continue; end
+    if st.isdir, continue; end
+    suff = st.name((nameLen-suffLen+1):end);
+    if strcmp(fileSuff, suff)
+        idx(end+1)=i;
+    end
+end
+dir_struct = dir_struct(idx);
