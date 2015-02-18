ccff14477adfa0203520498ae7ea1e25e3b43e90
Roswitha Gostner <r.gostner@gmail.com>
2015-02-18 10:42:18 +0000

    forcefully close wait bar.

--- 1-ImageSegmentationFull/Pre_ProcessMovies.m
+++ 1-ImageSegmentationFull/Pre_ProcessMovies.m
@@ -1,17 +1,21 @@
 function  Pre_ProcessMovies(  )
-% out= ConfigPane1;
-% folder = out.folder;
-% files = out.files;
-folder='/Users/iasmam/Desktop/Sam_Project/DV_Files/';
-files ={'GLA6_siKif1C45_siCon88_54hRNAi_exp1_08_R3D.dv'};
-maxFrame=1;
-for i = 1:length(files)
+out= ConfigPane1;
+display(out);
+folder = out.folder;
+files = out.files;
+maxFrame=inf;
+len = length(files);
+h = waitbar(0,'loading next graphical interface...');
+for i = 1:len
     path = fullfile(folder, files{i});
     metadat = imreadBFmeta(path);
     frames = metadat.nframes;
-    maxFrame=max(frames, maxFrame);
+    maxFrame=min(frames, maxFrame);
+    waitbar(i/len, 'loading next graphical interface...');
 end
-
-display('Now we know max frames:');
-display(maxFrame)
+delete(h)
+close all force
+ display('after first gui ')
+outConfig2=ConfigPane2(maxFrame);
+ display(outConfig2);
 
