40f3e76786962f7a667629c1880d6a93602dd3fb
rose <r.gostner@gmail.com>
2015-03-08 18:31:42 +0000

    no message

---
+++ 3-Extended_Affinity_Propagation/Inspect_ShapeAffinity.m
@@ -0,0 +1,17 @@
+function   Inspect_Shapes()
+folder = uigetdir(matlabroot,'Select Analysis Folder');
+dataFile = fullfile(folder, 'CellShapeData.mat');
+if exist(dataFile, 'file')
+    data = load(dataFile);
+    CellShapeData= data.CellShapeData;
+    unordered_list(CellShapeData, folder)
+else 
+    display('-------')
+    display('The file "Bigcellarrayandindex.mat" does not exist in your Analysis folder.');
+    display('Please check whether previous steps have been succesfully completed.');
+    display('-------')
+    exit(0);
+end
+
+end
+
---
+++ UiComponents/OoseConfig.m
@@ -0,0 +1,188 @@
+function varargout = OoseConfig(varargin)
+% OOSECONFIG MATLAB code for OoseConfig.fig
+%      OOSECONFIG, by itself, creates a new OOSECONFIG or raises the existing
+%      singleton*.
+%
+%      H = OOSECONFIG returns the handle to a new OOSECONFIG or the handle to
+%      the existing singleton*.
+%
+%      OOSECONFIG('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in OOSECONFIG.M with the given input arguments.
+%
+%      OOSECONFIG('Property','Value',...) creates a new OOSECONFIG or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before OoseConfig_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to OoseConfig_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help OoseConfig
+
+% Last Modified by GUIDE v2.5 06-Mar-2015 15:18:17
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @OoseConfig_OpeningFcn, ...
+                   'gui_OutputFcn',  @OoseConfig_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before OoseConfig is made visible.
+function OoseConfig_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to OoseConfig (see VARARGIN)
+
+% Choose default command line output for OoseConfig
+handles.output = hObject;
+guidata(hObject, handles);
+cPath=mfilename('fullpath');
+[folder,~,~] = fileparts(cPath);
+imFile = fullfile(folder, 'img/', 'header.png');
+img=imread(imFile);
+imshow(img,'Parent',handles.axes1)
+set(handles.edit1, 'String','...') 
+set(handles.edit2, 'String','...')
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes OoseConfig wait for user response (see UIRESUME)
+uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = OoseConfig_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+struc.folder = get(handles.edit1, 'String');
+struc.folder = get(handles.edti1, 'String');
+varargout{1}=struc;
+delete(handles.figure1);
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+folder =uigetdir(matlabroot,'Select Analysis Directory');
+set(handles.edit1, 'String',folder)  
+set(handles.pushbutton2, 'enable', 'on'); 
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+folder =get(handles.edit1, 'String');
+if  ~exist(folder, 'dir')
+    mode = struct('WindowStyle','non-modal','Interpreter','tex');
+    msg = DialogMessages(5);
+    errordlg(msg, 'Error', mode);
+else
+    figure1_CloseRequestFcn(hObject, eventdata, handles)
+end
+
+
+
+
+
+
+
+
+% --- Executes when user attempts to close figure1.
+function figure1_CloseRequestFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: delete(hObject) closes the figure
+h= handles.figure1;
+if isequal(get(h, 'waitstatus'), 'waiting')
+    uiresume(h)
+else
+    h= handles.figure1;
+    delete(h);
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+folder =uigetdir(matlabroot,'Select Analysis Directory');
+set(handles.edit2, 'String',folder)  
+set(handles.pushbutton2, 'enable', 'on'); 
+
+
