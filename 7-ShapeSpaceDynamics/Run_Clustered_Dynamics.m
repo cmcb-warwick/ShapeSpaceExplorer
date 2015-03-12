22606113beea4748968f843ae9f6c0ac6241bac8
rose <r.gostner@gmail.com>
2015-03-12 08:29:03 +0000

    added number 3 of the Dynamic Data in user friendly form.

---
+++ 7-ShapeSpaceDynamics/Run_Clustered_Dynamics.m
@@ -0,0 +1,51 @@
+function  Run_Clustered_Dynamics()
+    out=ConstrainedClustering();
+    folder =out.fpath;
+    classes = out.classes;    
+cellShapePath = fullfile(folder, 'CellShapeData.mat');
+if exist(cellShapePath, 'file')
+    try data = load(cellShapePath);
+        cellShapeData=data.CellShapeData;
+    catch
+        fileHasWrongStructure(cellShapePath);
+        return;
+    end
+else
+    filleDoesNotexist(cellShapePath);
+    return;
+end
+
+dynamicPath = fullfile(folder, 'DynamicData.mat');
+if exist(dynamicPath, 'file')
+    try dData = load(dynamicPath);
+        dynamicData=dData.DynamicData;
+    catch
+        fileHasWrongStructure(dynamicPath);
+        return;
+    end
+else
+    filleDoesNotexist(dynamicPath);
+    return;
+end
+
+
+Exemplar_direction_rose_plots(dynamicData, cellShapeData, classes, folder);
+display('Clustered Dynamic Data run successfully');
+display('-------');
+end
+
+
+
+function filleDoesNotexist(filename)
+    display('-------');
+    display(['The file "' filename '" does not exist in your Analysis folder.']);
+    display('Please check whether previous steps have been succesfully completed.');
+    display('-------');
+end
+
+function fileHasWrongStructure(filename)
+    display('-------');
+    display(['The file "' filename '" does not have the expected structure in your Analysis folder.']);
+    display('Please check whether previous steps have been succesfully completed.');
+    display('-------');
+end
