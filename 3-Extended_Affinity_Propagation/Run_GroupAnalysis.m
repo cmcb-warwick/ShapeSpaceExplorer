ea87861850f734a76bd8c83d42819617ff1d73ff
Roswitha Gostner <r.gostner@gmail.com>
2015-04-10 12:41:47 +0100

    group analysis plotting clusters kind of working.

--- 3-Extended_Affinity_Propagation/Run_GroupAnalysis.m
+++ 3-Extended_Affinity_Propagation/Run_GroupAnalysis.m
@@ -1,26 +1,129 @@
-function   runGroupAnalysis( input_args )
+function   runGroupAnalysis()
 
 
-folder ='/Users/iasmam/Desktop/TestSam';
-cluster=4;
+inputFolder ='/Users/iasmam/Desktop/Analysis';
+number=4;
+items={};
 str.name = 'Group1';
 str.tracks=[1];
-str.id=1,
-items={};
+str.id=1;
 items{1}=str;
-s.name = Group2;
+
+
+s.name = 'Group2';
 s.tracks=[2];
 s.id=2;
-items{2}=str;
-t.name = Group2;
-t.tracks=[2];
-t.id=2;
-items{2}=str;
+items{2}=s;
+
+t.name = 'Group3';
+t.tracks=[3];
+t.id=3;
+items{3}=t;
 
 %----------------------%
+load(fullfile(inputFolder, '/APclusterOutput.mat'));
+load(fullfile(inputFolder, '/wish_list.mat'));
+load(fullfile(inputFolder, '/linkagemat.mat'));
+load(fullfile(inputFolder, '/CellShapeData.mat'));
+load(fullfile(inputFolder, '/Bigcellarrayandindex.mat'));
+load(fullfile(inputFolder, '/BigCellDataStruct.mat'));
+
+groupPath=fullfile(inputFolder, 'GroupAnalysis');
+
+
+if ~exist(groupPath,'dir'),mkdir(groupPath);end
+
+
+N=length(CellShapeData.point);
+
+if isfield(CellShapeData.set,'SCORE')
+    SCORE=CellShapeData.set.SCORE;
+else
+    for i=1:N
+       SCORE(i,:)= CellShapeData.point(i).SCORE;
+    end
+end
+
+
+for i=1:N
+    NewCellArray{i}=CellShapeData.point(i).coords_comp;
+end
+
+
+colour=jet(number);
+colour=flipud(colour);
+colour=colour.*repmat((1-0.25*colour(:,2)),1,3);
+
+
+n_exems=length(wish_list);
+exem_list=sort(wish_list);
+
+figure('visible','off')
+[~,T]=dendrogram(linkagemat,number);
+if max(T(:))<number
+    mode = struct('WindowStyle','non-modal','Interpreter','tex');
+    msg = DialogMessages(2);
+    errordlg(msg, 'Error', mode);
+    return
+end
+    
+    
+for i=1:n_exems
+T2(i)=T(exem_list==wish_list(i));
+end
+
+d=diff([0 T2]);
+clust_order=T2(logical(d));
+
+figure
+for i=1:number
+    clust_idx=clust_order(i);
+    exems=wish_list(T2==clust_idx);
+    points=ismember(idx,exems);
+    plot(SCORE(points,1),SCORE(points,2),'o','Color',colour(i,:), 'MarkerSize', 5)
+    hold on
+end
+axis tight
+axis equal
+grid on
 
 
 
+
+stack_indices=getStackIndices(BigCellDataStruct);
+gIds =getAllIndicesFor(stack_indices, [2]);
+for i=1:number
+    clust_idx=clust_order(i);
+    exems=wish_list(T2==clust_idx);
+    points=ismember(idx.*gIds,exems);
+    plot(SCORE(points,1),SCORE(points,2),'.','Color',colour(i,:), 'MarkerSize', 14)
+    hold on
+end
+% till here we plot all shapes. no group hightlighted.
 
 end
 
+
+function indices =getAllIndicesFor(stack_indices, stacks)
+indices =zeros(size(stack_indices));
+    for i=1:length(stacks)
+        idx=find(stack_indices==stacks(i));
+        indices(idx)=1;
+    end
+end
+
+
+
+
+function stack_indices=getStackIndices(BigCellDataStruct)
+idx=1;
+stack_indices=[];
+s=size(BigCellDataStruct);
+for i =1:s(2)
+    item = BigCellDataStruct(i);
+    shapes = size(item.Contours);
+    stack_indices(idx:idx+shapes(2)-1)=item.Stack_number;
+    idx=idx+shapes(2);
+end
+stack_indices=stack_indices';
+end
