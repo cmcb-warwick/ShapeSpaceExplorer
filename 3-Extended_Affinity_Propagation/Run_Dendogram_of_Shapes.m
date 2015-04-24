f1514332c3bc9c72e797a2f1cd1965a3db3f10f3
Roswitha Gostner <r.gostner@gmail.com>
2015-04-24 16:23:38 +0100

    corrected Typo.

--- 3-Extended_Affinity_Propagation/Run_Dendogram_of_Shapes.m
+++ 3-Extended_Affinity_Propagation/Run_Dendogram_of_Shapes.m
@@ -1,12 +1,12 @@
 function  Run_Dendogram_of_Shapes( )
-folder = uigetdir(matlabroot,'Select Experiment Folder');
+folder = uigetdir(matlabroot,'Select Analysis Folder');
 dataFile = fullfile(folder, 'CellShapeData.mat');
 if exist(dataFile, 'file')
     data = load(dataFile);
     ordered_list(0,data.CellShapeData, folder, folder);
 else 
     display('-------')
-    display('The file "CellShapeData.mat" does not exist in your Experiment folder.');
+    display('The file "CellShapeData.mat" does not exist in your Analysis folder.');
     display('Please check whether previous steps have been succesfully completed.');
     display('-------')
 
