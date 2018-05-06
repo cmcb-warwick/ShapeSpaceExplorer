291d055de8dde588f8d70cccd98dae0fffa4f4b6
mohammed <none@none>
2018-05-06 12:38:45 +0100

    sub function for slim cell shape data generation for OOSE (new file added)

---
+++ 6-OoSE/slimCellShapeDataOoSE.m
@@ -0,0 +1,21 @@
+% slims down the huge CellShapeData.mat output to 
+% what is necessary in the rest of the analysis.
+% if true is added
+function [ CellShapeData_slim ] = slimCellShapeDataOoSE( CellShapeData,scoress, XtraSlim )
+CellShapeData_slim.set={};
+CellShapeData_slim.point={};
+CellShapeData_slim.set.SCORE=CellShapeData.point.SCORE;
+CellShapeData_slim.set.Long_D=CellShapeData.set.Long_D;%
+N = length(scoress.OoSE_emb);
+for i =1:N
+    CellShapeData_slim.set.SCORE(i,:) = scoress.OoSE_emb(i,:);
+    CellShapeData_slim.point(i).SCORE =CellShapeData.point(i).SCORE;
+    CellShapeData_slim.point(i).coords_comp= CellShapeData.point(i).coords_comp;
+    if nargin<2 % no extra slim
+    CellShapeData_slim.point(i).sum_curve= CellShapeData.point(i).sum_curve;
+    CellShapeData_slim.point(i).fft_flip= CellShapeData.point(i).fft_flip;
+    CellShapeData_slim.point(i).fft_conj= CellShapeData.point(i).fft_conj;
+    end
+end
+
+end
