72934a7c54bc80b20e28d01e5e600d0497142dc4
Laura Cooper <l.cooper.5@warwick.ac.uk>
2022-10-07 15:51:13 +0100

    fixed bugs caused by wrong file and variable names and reformatted results

--- 1-ImageSegmentationFull/MakeBigStructsWithoutManual.m
+++ 1-ImageSegmentationFull/MakeBigStructsWithoutManual.m
@@ -4,9 +4,9 @@ global PATH
 if (length(PATH)>2)
 root= PATH;
 end
-folder = uigetdir(root,'Select Experiment Folder');
+folder = uigetdir(root,'Select Analysis Folder');
 PATH=folder;
-dirData=dir( fullfile(folder,'CellFrameData*.mat') );
+dirData=dir( fullfile(folder,'ImageStack*CurveData.mat') );
 dirIndex = [dirData.isdir];  %# Find the index for directories
 fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
 if ~isempty(fileList)
@@ -14,29 +14,34 @@ if ~isempty(fileList)
     fileList,'UniformOutput',false);
 end
 N = length(fileList);
-if N<1, error('No Files found in Experiment folder, stopped without processing');
+if N<1, error('No Files found in Analysis folder, stopped without processing');
     exit(0); end
-
+    
 BigCellDataStruct=[];
+BigCellArray=[];
+cell_indices=[];
 for i=1:N
-    load(sprintf('ImageStack%03dCurveData.mat',i))
+    load(fullfile(folder,sprintf('ImageStack%03dCurveData.mat',i)))
     num_cells=max(cell2mat(Cell_numbers));
     num_frames=length(Frame_curves);
     for j=1:num_cells
+        CellArray=[];
         for k=1:num_frames
-            CellArray={};
             if ismember(j,Cell_numbers{k})
-                BigCellArray=[BigCellArray Frames_curves{k}(Cell_numbers{k}==j)];
-                CellArray=[CellArray Frames_curves{k}(Cell_numbers{k}==j)];
-                cell_indices=[cell_indices j+running_cell_indices];
+                BigCellArray=[BigCellArray; Frame_curves{k}(Cell_numbers{k}==j)];
+                CellArray=[CellArray Frame_curves{k}(Cell_numbers{k}==j)];
+                cell_indices=[cell_indices; j];
             end
-            CellDataStruct=struct('Stack_number',i,'Cell_numbers',k,'Contours',CellArray);
+            CellDataStruct=struct('Stack_number',i,'Cell_number',j,'Contours',{CellArray}, 'BadFrames', []);
         end
-        BigCellDataStruct=[BigCellDataStruct, CellFrameData];
+    BigCellDataStruct=[BigCellDataStruct, CellDataStruct];
     end
     
+
 end
 
-save -v7.3 BigCellDataStruct.mat BigCellDataStruct
+sFile = fullfile(folder, 'BigCellDataStruct.mat');
+save( sFile, 'BigCellDataStruct', '-v7.3');
 
-save('Bigcellarrayandindex.mat', 'BigCellArray', 'cell_indices', '-v7.3')
+sFile = fullfile(folder, 'Bigcellarrayandindex.mat');
+save(sFile, 'BigCellArray', 'cell_indices', '-v7.3');
