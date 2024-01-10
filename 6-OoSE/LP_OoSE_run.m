9761d400a7fcc998c41c37ceb0d037c28fa19408
Laura190 <l.cooper.5@warwick.ac.uk>
2024-01-10 19:10:58 +0000

    assign variables and allow saving for all target_funcs

--- 6-OoSE/LP_OoSE_run.m
+++ 6-OoSE/LP_OoSE_run.m
@@ -82,18 +82,23 @@ ndims=length(d);
 filedir = fullfile(new_unique_savedestination, 'Dist_mat.mat');
 save(filedir, 'D','-v7.3');
 D=D.^2;
-[l,~]=size(d);
+
+target_func=trainingCellShapeData.set.SCORE;
+for n=1:length(d)
+[l,~]=size(d{n});
+ftrain = target_func(:,n)';
 for m=0:l
     sig = sig0/(2^m);
     %equation 3.17
-    gly = exp(-D/sig);
+    gly = exp(-D'/sig);
     qly = sum(gly,2);
     kly = (qly.^(-1)).*gly;
     if m==0
-        fy = sum(kly.*ftrain,2)';
+        fy{n} = sum(kly.*ftrain,2)';
     else
-        fy = fy+sum(kly.*d(m,:),2)';
+        fy{n} = fy{n}+sum(kly.*d{n}(m,:),2)';
     end
+end
 end
 
 OoSE_emb=fy;
