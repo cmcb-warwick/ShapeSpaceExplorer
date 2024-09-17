d92c6669c2d2804073ae7553e4d6494bfaa2149f
laura <l.cooper.5@warwick.ac.uk>
2024-09-17 09:49:20 +0100

    clear vars and close figures

--- 3-ShapeFeatures/Run_PlotBAMvsSCORES.m
+++ 3-ShapeFeatures/Run_PlotBAMvsSCORES.m
@@ -9,7 +9,7 @@ BPath=fullfile(inputFolder, 'Figures','Validation');
 if ~exist(BPath,'dir'),mkdir(BPath);end
 %----------------------%
 display('Files are loading ... ');
-load(fullfile(inputFolder, '/CellShapeData.mat'));
+load(fullfile(inputFolder, '/CellShapeData_slim.mat'));
 load(fullfile(inputFolder, '/Morphframe.mat'));
 display('Files loaded ');
 
@@ -17,12 +17,16 @@ display('Files loaded ');
 
 YY=[];
 ZZ=[];
-for i=1:length(morphframe)
+train_nobs=length(morphframe);
+for i=1:train_nobs
     YY=[YY; morphframe(i).Solidity morphframe(i).Dist_ratio ];
     ZZ=[ZZ; CellShapeData.set.SCORE(i,1) CellShapeData.set.SCORE(i,2)];
 end
+D=CellShapeData.set.Long_D;
+clear('CellShapeData');
+clear('morphframe');
     
-train_nobs=length(morphframe);
+
 DD=zeros(train_nobs,train_nobs);
 DDZ=zeros(train_nobs,train_nobs);
 for i=1:train_nobs
@@ -31,14 +35,11 @@ for i=1:train_nobs
         DDZ(i,j)=sqrt((ZZ(i,1)-ZZ(j,1))^2 + (ZZ(i,2)-ZZ(j,2))^2);
     end
 end
-
 mask=tril(ones(train_nobs,train_nobs),-1);
 Y=DD(logical(mask));
 clear('DD');
-Z=DDZ(logical(mask));
-clear('DDZ');
+clear('YY');
 
-D=CellShapeData.set.Long_D;
 D2=zeros(train_nobs,train_nobs);
 k=1;
 for j=2:train_nobs
@@ -48,16 +49,15 @@ for j=2:train_nobs
         k=k+1;
     end
 end
-D=D2;
+clear('D');
+X=D2(logical(mask));
 clear('D2');
 
-X=D(logical(mask));
-clear('D');
  p = polyfit(X,Y,1); 
  f = polyval(p,X);
 h=figure(1);
 reduce_plot(X,Y,'.',X,f,'-');
-xlabel('BAM Distance')
+xlabel('BAM Diclose(h);stance')
 ylabel('Solidity and DistRatio Distance')
 mn=mean(Y);
 dot(Y-mn,Y-mn); 
@@ -66,8 +66,35 @@ SSE=dot(Y-f,Y-f);
 Rsq=1-SSE/SST;
 title(['Coefficient of determination (Rsq) value is  ' num2str(Rsq) ])
       fPath=fullfile(BPath, 'plot_regression_solidyratio');
-      saveas(h, fPath, 'fig');
+      saveas(h, fPath, 'pdf');
       saveas(h, fPath, 'epsc')
+close(h);
+
+
+h=figure(111);
+[N,xedges,yedges] = histcounts2(X,Y,[100,100]);
+imagesc(xedges,yedges, N');
+xlabel('BAM'); ylabel('Solidity and DistRatio Distance');
+axis xy;
+colormap('hot')
+colorbar
+      fPath=fullfile(BPath, 'plot_density_solidyratio');
+      saveas(h, fPath, 'pdf');
+      saveas(h, fPath, 'epsc')
+hold on,
+reduce_plot(X,f,'b-');
+      fPath=fullfile(BPath, 'plot_density_solidyratioReg');
+      saveas(h, fPath, 'pdf');
+      saveas(h, fPath, 'epsc')
+hold off
+close(h);
+clear('Y');
+
+Z=DDZ(logical(mask));
+clear('DDZ');
+clear('ZZ');
+clear('f');
+
  p = polyfit(X,Z,1); 
  ff = polyval(p,X);
 h=figure(2);
@@ -81,43 +108,25 @@ xlabel('BAM')
 ylabel('SCORE Distance')
 title(['Coefficient of determination (Rsq) value is  ' num2str(Rsq) ])
       fPath=fullfile(BPath, 'plot_regression_Scores');
-      keyboard;
-      saveas(h, fPath, 'fig');
+      saveas(h, fPath, 'pdf');
       saveas(h, fPath, 'epsc');
-
-      
-%%
-h=figure(111);
-[N,xedges,yedges] = histcounts2(X,Y,[100,100]);
-imagesc(xedges,yedges, N');
-xlabel('BAM'); ylabel('Solidity and DistRatio Distance');
-axis xy;
-colormap('hot')
-colorbar
-      fPath=fullfile(BPath, 'plot_density_solidyratio');
-      saveas(h, fPath, 'fig');
-      saveas(h, fPath, 'epsc')
-hold on,
-reduce_plot(X,f,'b-');
-      fPath=fullfile(BPath, 'plot_density_solidyratioReg');
-      saveas(h, fPath, 'fig');
-      saveas(h, fPath, 'epsc')
-hold off,    
+close(h);
 
 h=figure(222);
-[N,xedges,yedges] = histcounts2(X,Y,[100,100]);
+[N,xedges,yedges] = histcounts2(X,Z,[100,100]);
 imagesc(xedges,yedges, N');
 xlabel('BAM'); ylabel('SCORE Distance');
 axis xy;
 colormap('hot')
 colorbar
 fPath=fullfile(BPath, 'plot_density_Scores');
-      saveas(h, fPath, 'fig');
+      saveas(h, fPath, 'pdf');
       saveas(h, fPath, 'epsc')
 hold on,
 reduce_plot(X,ff,'b-');
       fPath=fullfile(BPath, 'plot_density_ScoresReg');
-      saveas(h, fPath, 'fig');
+      saveas(h, fPath, 'pdf');
       saveas(h, fPath, 'epsc')
+close(h);
 
 end
